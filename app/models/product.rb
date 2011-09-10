17e45d354d93161e1b3cfaf8d85f64c01dfea8ab
Lanisle <www.zvv.cn@gmail.com>
2011-09-11 00:21:14 +0800

    first commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,8 +5,12 @@ class Product < ActiveRecord::Base
 	validates_uniqueness_of :title
 	validates_length_of :title, :minimum => 10, :too_short => 'Length of title at least 10 chars!'
 	validates_format_of :image_url,
-											:with => %r{\.(gif|jpg|png)$}i,
-											:message => 'must be a URL for GIF, JPG or PNG image'
+		:with => %r{\.(gif|jpg|png)$}i,
+		:message => 'must be a URL for GIF, JPG or PNG image'
+	public
+		def self.find_products_for_sale
+			find(:all, :order => 'title')
+		end
 	
 	protected
 		def price_must_be_at_least_a_cent
